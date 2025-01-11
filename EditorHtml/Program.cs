0ab85bd3a2995148c19caa7fce2db7f6eb947218
Mariana Moreira <marimoreirabsi@gmail.com>
2025-01-11 18:58:51 -0300

    feat : Projeto Editor de HTML

---
+++ EditorHtml/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace EditorHtml
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Menu.Show();
+
+        }
+    }
+}
